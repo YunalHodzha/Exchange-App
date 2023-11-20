1230aee767f85e83ddcc73002ed0b521fac64a53
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-11-20 13:44:40 +0200

    add index.html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
 
 //routes
 app.get('/', (req, res) => {
-    res.send('Hello Node API');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
