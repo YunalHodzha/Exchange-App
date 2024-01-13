642c20ec86884f95b4f77aa422e8f5fc8170bc8d
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2024-01-13 21:19:25 +0200

    update

--- src/config/dbConfing.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const uri = 'mongodb://127.0.0.1:27017/test';
-
-async function dbConnect() {
-    await mongoose.connect(uri);
-};
-
-module.exports = dbConnect;
